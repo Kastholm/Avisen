0630b6ed9f07b3a24e46b682c3558b67e1407918
Kastholm <marc@aguardio.com>
2024-02-23 17:50:46 +0100

    nav logic

--- components/MyNavBar.jsx
+++ components/MyNavBar.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { Stack, Card, Flex, Text } from '@sanity/ui';
 import logo from '../static/logo.png';
 import '../stylesheets/MyNavBar.css';
-import navData from '../json/MyNavBar.json'
+
 
 function MyNavbar(props) {
 
-  const medier = [
-    { "name": "Dagens Avis 1", "url": "URL_TIL_AVIS1" },
-    { "name": "Dagens Avis 2", "url": "https://bellaitalia.sanity.studio/structure" },
-    { "name": "Dagens Avis 3", "url": "URL_TIL_AVIS3" }
-  ];
+  const [navItems, setNavItems] = useState();
+  const [showNavItems, setShowNavItems] = useState(false)
+
+  useEffect(() => {
+    const medier = [
+      { "name": "___________", "url": "#" },
+      { "name": "Nyheder24", "url": "URL_TIL_AVIS1" },
+      { "name": "___________", "url": "#" },
+      { "name": "Dagens.de", "url": "https://bellaitalia.sanity.studio/structure" },
+      { "name": "Dagens.se", "url": "URL_TIL_AVIS3" },
+      { "name": "Dagens.no", "url": "URL_TIL_AVIS3" },
+      { "name": "Dagens.dk", "url": "URL_TIL_AVIS3" }
+    ];
+
+setNavItems(medier);
+setShowNavItems(true);
+  }, [])
+
+  
+
 
   // Denne funktion kaldes, når brugeren vælger en ny option
   const handleSelectChange = (event) => {
@@ -28,15 +44,33 @@ function MyNavbar(props) {
         <img src={logo} alt="Logo" />
         <div className='searchContainer'>
           <h2>Skift til andet medie:</h2>
-          <select name="dagensAvis" id="dagensAvisSelect" onChange={handleSelectChange}>
+
+          {
+            showNavItems === true ? (<select name="dagensAvis" id="dagensAvisSelect" onChange={handleSelectChange}>
             <option value="">Vælg et medie</option> 
-            {medier.map((media) => (
-              <option key={media.url} value={media.url}>{media.name}</option>
+            {navItems.map((media) => (
+              <option key={media.url} value={media.url}><img src={logo} alt="Logo" />{media.name}</option>
             ))}
-          </select>
+          </select>) : null
+          }
+        
 
         <button onClick={() => chooseMedia()} >Vælg Medier</button>
+        <div className='toggleBox'>
+  {showNavItems === true ? (
+    <div>
+      {navItems.map((item) => (
+        <p key={item.url}>{item.url}</p> // Tilføj en return her
+      ))}
+    </div>
+  ) : null}
+</div>
+
         </div>
+
+
+
+
       </nav>
       {props.renderDefault && props.renderDefault(props)}
     </Stack>
